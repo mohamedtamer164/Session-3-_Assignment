4dc9a6051df828dfd41e726cdc893fdbcb636ee7
original <original@DESKTOP-H4JMMMN>
2026-09-09 13:03:16 +0300

    answer 1

--- Session 3 _Assignment/Program.cs
+++ Session 3 _Assignment/Program.cs
@@ -5,7 +5,16 @@
         static void Main(string[] args)
         {
             // ANSWER 1
-            
+            #region String 
+            string Title = "clean code";
+            string upperTitle = Title.ToUpper();
+
+            Console.WriteLine(upperTitle); //  هنا هيطلع الكود كله حلاوف كبيره       
+            Console.WriteLine(Title); //  هيطلع الكود زي م هو 
+            Console.WriteLine(Title.GetHashCode()); 
+            Console.WriteLine(upperTitle.GetHashCode()); //  هنا عاوز اشوف هل العنوان قيمته اتغيرت ولا لا 
+
+            #endregion
         }
     }
 }
